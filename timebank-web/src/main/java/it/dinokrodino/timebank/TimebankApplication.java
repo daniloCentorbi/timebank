b0ab837afc653ee60b49042c3ba2d2a0804009bb
dani <danilo.centorby@gmail.com>
2019-06-23 16:31:42 +0700

    refactoring to multimodule mvn project

--- timebank-web/src/main/java/it/dinokrodino/timebank/TimebankApplication.java
+++ timebank-web/src/main/java/it/dinokrodino/timebank/TimebankApplication.java
@@ -1,4 +1,4 @@
-package co.uk.dinokrodino.timebank;
+package it.dinokrodino.timebank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
